38928d6abc83440951e37701f98de7aa42be20a1
Alexis-Dk <alexeydruzik@inbox.ru>
2020-02-08 16:24:18 +0300

    Added entry point for Spring-boot.

---
+++ module-testing-group-project-rest/src/main/java/com/moduleTesting/rest/ModuleTestingApplication.java
@@ -0,0 +1,11 @@
+package com.moduleTesting.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleTestingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleTestingApplication.class, args);
+    }
+}
