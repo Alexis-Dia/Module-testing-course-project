11ad0c28e321df2d84f7e5dc99e9516ea26eb488
Alexis-Dk <alexeydruzik@inbox.ru>
2020-02-17 12:05:01 +0300

    Added necessary car services.

--- module-testing-group-project-service/src/main/java/com/moduleTesting/portal/service/car/CarService.java
+++ module-testing-group-project-service/src/main/java/com/moduleTesting/portal/service/car/CarService.java
@@ -1,10 +1,18 @@
 package com.moduleTesting.portal.service.car;
 
+import com.moduleTesting.portal.dto.BrandDto;
+import com.moduleTesting.portal.dto.CarStatus;
 import com.moduleTesting.portal.entity.CarEntity;
 
+import java.util.Date;
 import java.util.List;
 
 public interface CarService {
 
     List<CarEntity> findAll();
+
+    void editCar(Integer carId, BrandDto brandId, Date year, String number, Date dateOfReceipt, CarStatus carStatus);
+
+    void removeCar(Integer carId);
+
 }
--- module-testing-group-project-service/src/main/java/com/moduleTesting/portal/service/car/impl/CarServiceImpl.java
+++ module-testing-group-project-service/src/main/java/com/moduleTesting/portal/service/car/impl/CarServiceImpl.java
@@ -1,11 +1,14 @@
 package com.moduleTesting.portal.service.car.impl;
 
+import com.moduleTesting.portal.dto.BrandDto;
+import com.moduleTesting.portal.dto.CarStatus;
 import com.moduleTesting.portal.entity.CarEntity;
 import com.moduleTesting.portal.repository.CarRepository;
 import com.moduleTesting.portal.service.car.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -22,4 +25,14 @@ public class CarServiceImpl implements CarService {
     public List<CarEntity> findAll() {
         return carRepository.findAll();
     }
+
+    @Override
+    public void editCar(Integer carId, BrandDto brandId, Date year, String number, Date dateOfReceipt, CarStatus carStatus) {
+    }
+
+    @Override
+    public void removeCar(Integer carId) {
+
+    }
+
 }
