c21c6ee98be5b28e99aacc9367a2082d7a8367ad
Alexis-Dk <alexeydruzik@inbox.ru>
2020-02-17 12:26:02 +0300

    Added necessary report services.

--- module-testing-group-project-service/src/main/java/com/moduleTesting/portal/service/report/ReportService.java
+++ module-testing-group-project-service/src/main/java/com/moduleTesting/portal/service/report/ReportService.java
@@ -2,9 +2,15 @@ package com.moduleTesting.portal.service.report;
 
 import com.moduleTesting.portal.entity.ReportEntity;
 
+import java.util.Date;
 import java.util.List;
 
 public interface ReportService {
 
     List<ReportEntity> findAll();
+
+    List<ReportEntity> getReportsByTaskId(Integer taskId);
+
+    void createReport(Date departure, Float weight, Float distance, Date arrival);
+
 }
--- module-testing-group-project-service/src/main/java/com/moduleTesting/portal/service/report/impl/ReportServiceImpl.java
+++ module-testing-group-project-service/src/main/java/com/moduleTesting/portal/service/report/impl/ReportServiceImpl.java
@@ -6,6 +6,7 @@ import com.moduleTesting.portal.service.report.ReportService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -18,4 +19,14 @@ public class ReportServiceImpl implements ReportService {
     public List<ReportEntity> findAll() {
         return reportRepository.findAll();
     }
+
+    @Override
+    public List<ReportEntity> getReportsByTaskId(Integer taskId) {
+        return null;
+    }
+
+    @Override
+    public void createReport(Date departure, Float weight, Float distance, Date arrival) {
+    }
+
 }
