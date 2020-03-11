a54a4a2dd7853754c68640d2cef72bd19431f6af
Alexis-Dk <alexeydruzik@inbox.ru>
2020-03-11 18:03:00 +0300

    Committed tests for user-layer. Part 4.

--- module-testing-group-project-service/src/test/java/com/moduleTesting/portal/service/task/impl/TaskServiceImplTest.java
+++ module-testing-group-project-service/src/test/java/com/moduleTesting/portal/service/task/impl/TaskServiceImplTest.java
@@ -254,6 +254,16 @@ public class TaskServiceImplTest {
         verifyNoMoreInteractions(taskStatusRepository);
     }
 
+    @Test
+    public void testCreateNewTask_Ok() {
+        taskService.createNewTask(TASK_DTO);
+
+        verify(taskRepository, Mockito.times(WANTED_NUMBER_OF_INVOCATIONS_ONE_TIME)).save(ArgumentMatchers.any(TaskEntity.class));
+        verify(taskRepository, Mockito.times(WANTED_NUMBER_OF_INVOCATIONS_ONE_TIME)).findAll();
+
+        verifyNoMoreInteractions(taskRepository);
+    }
+
     @Test(expected= TaskNotFoundException.class)
     public void testTakeTask_Exception_TaskNotFound() {
         taskService.takeTask(NOT_EXISTED_TASK_ID, EXISTED_CAR_ID, EXISTED_EMAIL);
@@ -273,13 +283,4 @@ public class TaskServiceImplTest {
         verifyNoMoreInteractions(taskStatusRepository);
     }
 
-    @Test
-    public void testCreateNewTask_Ok() {
-        taskService.createNewTask(TASK_DTO);
-
-        verify(taskRepository, Mockito.times(WANTED_NUMBER_OF_INVOCATIONS_ONE_TIME)).save(ArgumentMatchers.any(TaskEntity.class));
-        verify(taskRepository, Mockito.times(WANTED_NUMBER_OF_INVOCATIONS_ONE_TIME)).findAll();
-
-        verifyNoMoreInteractions(taskRepository);
-    }
 }
